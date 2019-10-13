09fa7f3b7f71c9dacb04bc6ecb5a0ea4b703c2eb
Alvaro Furtado <alvao4ebis@hotmail.com>
2019-10-13 20:27:45 -0300

    Aprendendo React

---
+++ app/components/app.jsx
@@ -0,0 +1,23 @@
+import React, {createElement, useState} from "react";
+import { format } from "util";
+
+export default function app (props) { 
+    var [list, setList] = useState([]);
+    var [Newitem, setNewitem] = useState("")
+
+    var addItem = () => {setList (list.concat([Newitem]))}
+
+
+    return <main>
+        <header><h1>To-do {Newitem}</h1> </header>
+        <section>
+            <input placeholder="insira aqui sua tarefa"
+            value={Newitem} onChange={(e) => setNewitem (e.target.value)}/>
+            <button onClick={addItem}>Adicionar Item</button>
+
+            <ul> {list.map ((item)=> <li>{item}</li>)
+            }
+            </ul>
+        </section>
+    </main>
+}
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React, {createElement} from "react";
+import ReactDOM,{render} from "react-dom";
+import App from "./components/App";
+
+var main = document.querySelector("main");
+render (<App/>, main);
